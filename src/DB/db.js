9e6bb487660cfde5fe95dee13a1901ca828ff89d
RVCH-77 <riveravalderramachristopherisa@gmail.com>
2025-11-06 10:17:29 -0600

    Cambios en el nombre de la base de datos

--- src/DB/db.js
+++ src/DB/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '1234',
-  database: process.env.DB_NAME || 'cajero',
+  database: process.env.DB_NAME || 'cajero12',
   port: parseInt(process.env.DB_PORT || '3306', 10),
   waitForConnections: true,
   connectionLimit: 10,
